db05be9cc9ad41c0e2e490048edbaa1686758c96
anhxuan2712 <nguyenanhxuan2712@gmail.com>
2026-08-27 16:02:29 +0700

    bai tap 3.2

---
+++ bai 3.2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a, b, c;
+
+        Console.Write("Nhap a: ");
+        a = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhap b: ");
+        b = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhap c: ");
+        c = double.Parse(Console.ReadLine());
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                if (c == 0)
+                    Console.WriteLine("Phuong trinh co vo so nghiem.");
+                else
+                    Console.WriteLine("Phuong trinh vo nghiem.");
+            }
+            else
+            {
+                double x = -c / b;
+                Console.WriteLine($"Nghiem x = {x:F2}");
+            }
+        }
+        else
+        {
+            double delta = b * b - 4 * a * c;
+
+            if (delta > 0)
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                Console.WriteLine($"x1 = {x1:F2}");
+                Console.WriteLine($"x2 = {x2:F2}");
+            }
+            else if (delta == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine($"Nghiem kep x = {x:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Vo nghiem.");
+            }
+        }
+    }
+}
