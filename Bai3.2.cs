bd2db3ca643f6928ab3632a9afd87028a311880a
anhxuan2712 <nguyenanhxuan2712@gmail.com>
2026-08-27 15:17:26 +0700

    bai tap

---
+++ Bai3.2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a, b, c;
+
+        Console.Write("Nhập a: ");
+        a = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhập b: ");
+        b = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhập c: ");
+        c = double.Parse(Console.ReadLine());
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                if (c == 0)
+                    Console.WriteLine("Phương trình có vô số nghiệm.");
+                else
+                    Console.WriteLine("Phương trình vô nghiệm.");
+            }
+            else
+            {
+                double x = -c / b;
+                Console.WriteLine($"Nghiệm x = {x:F2}");
+            }
+        }
+        else
+        {
+            double delta = b * b - 4 * a * c;
+
+            if (delta > 0)
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                Console.WriteLine($"x1 = {x1:F2}");
+                Console.WriteLine($"x2 = {x2:F2}");
+            }
+            else if (delta == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine($"Nghiệm kép x = {x:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Vô nghiệm.");
+            }
+        }
+    }
+}
---
+++ bai 3.1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhap b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhap phep toan: ");
+        char op = char.Parse(Console.ReadLine());
+
+        try
+        {
+            double kq = op switch
+            {
+                '+' => a + b,
+                '-' => a - b,
+                '*' => a * b,
+                '/' => a / b,
+                '%' => a % b,
+                _ => 0
+            };
+
+            if ((op == '/' || op == '%') && b == 0)
+                throw new DivideByZeroException();
+
+            Console.WriteLine($"Ket qua: {kq:F2}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Loi: Khong the chia cho 0!");
+        }
+    }
+}
+
